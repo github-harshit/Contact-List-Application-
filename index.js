0590c95a1439899a56ec12dc0f41ccd4498f9f1f
Harshit Joshi <harshitjoshi250@gmail.com>
2023-01-14 00:27:50 +0530

    final commit

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const { urlencoded } = require('body-parser');
 const express = require('express'); 
+const db = require("./config/mongoose"); 
+const Contact = require("./models/contact"); 
+
+
 const app = express(); 
 const port = 8000; 
 const path = require('path'); 
@@ -13,38 +17,57 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views")); 
 // controller for rendering the data if route is "/"
 app.get('/', function(req, res){
-    return res.render("home",
+   Contact.find({}, function(err, contacts){
+    if(err){
+      console.log('error in finding contacts from db'); 
+       return; 
+   }
+     return res.render("home",
      { title: "my title",
-     contact_list :contactList
+     contact_list :contacts
 
     }
     )
+   })
+    
 })
 
 // controller for submiiting data from form and in which route we are submitting create_contact
 app.post("/create_contact", function(req, res){
    
-    var obj =  {
-        "name": req.body.name,
-        "no": req.body.phone
-     }
-     contactList.push(obj); 
-     return res.redirect("/"); 
+    //var obj =  {
+    //   "name": req.body.name,
+     //   "no": req.body.phone
+    // }
+    // contactList.push(obj);
+  Contact.create({
+    name: req.body.name, 
+    phone:req.body.phone
+  }, function(err, newContact){
+    if(err){
+     console.log("err"); 
+     return; 
+    }
+    console.log("*******" + newContact); 
+     return res.redirect('back'); 
+
+  })  ;
+     
 
 }); 
 
 //  controller for deleting the data 
 app.get("/delete_contact/", function(req, res){  // whatever is passed in query  after delete contact will be read as phone  
-   let phone = req.query.phone; 
-   console.log(phone); 
-   for(let i =0; i<contactList.length; i++){
-     console.log(contactList[i].no); 
-    if(contactList[i].no == phone){
-     
-        contactList.splice(i, 1); 
+   let id = req.query.id; 
+   Contact.findByIdAndDelete(id, function(err){
+    if(err){
+      console.log('something happened while deleting the database'); 
+      return; 
     }
-   } 
-   return res.redirect('/'); 
+
+     return res.redirect('/'); 
+   })
+  
 })
 
 
@@ -57,7 +80,7 @@ app.get("/delete_contact/", function(req, res){  // whatever is passed in query 
 
 app.listen(port, function(err){
     if(err){
-        console.lof(error); 
+        console.log(error); 
     }
      console.log("server is running "); 
 })
